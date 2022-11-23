05978e8b54e401076735ef7dd83295822b0af9ab
AsyaTiho <101018944+AsyaTiho@users.noreply.github.com>
2022-11-23 13:22:16 -0600

    Update std.function.unit3.3.py

--- std.function.unit3.3.py
+++ std.function.unit3.3.py
@@ -1,3 +1,26 @@
+"""
+Exercise 3
+5.0/5.0 points (graded)
+Write a function, stdDevOfLengths(L) that takes in a list of strings, L, and outputs the standard deviation of the lengths of the strings. Return float('NaN') if L is empty.
+
+Recall that the standard deviation is computed by this equation:
+
+where:
+
+ is the mean of the elements in X.
+
+ means the sum of the quantity  for t in X.
+
+That is, for each element (that we name t) in the set X, we compute the quantity . We then sum up all those computed quantities.
+
+N is the number of elements in X.
+
+Test case: If L = ['a', 'z', 'p'], stdDevOfLengths(L) should return 0.
+
+Test case: If L = ['apples', 'oranges', 'kiwis', 'pineapples'], stdDevOfLengths(L) should return 1.8708.
+
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 23 11:40:20 2022
